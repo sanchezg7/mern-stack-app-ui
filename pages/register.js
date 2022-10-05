e22ac23da8959c2835358ecd8a86f685175924c2
ayogsynergy <sanchezg.eng@gmail.com>
2022-10-04 21:34:34 -0400

    added a basic register form

--- pages/register.js
+++ pages/register.js
@@ -1,9 +1,51 @@
+import React, { useState } from "react";
 import Layout from "../components/Layout";
 
 const register = () => {
 
+    const [state, setState] = useState({
+        name: '',
+        email: '',
+        password: '',
+        error: '',
+        buttonText: 'Register'
+    });
+
+    const handleChange = e => {
+        e.preventDefault();
+        const newState = {...state};
+        const name = e.target.name;
+        const value = e.target.value;
+        newState[name] = value;
+        setState(newState);
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(e);
+    }
+
     return (
-        <Layout>Hi register</Layout>
+        <Layout>
+            <div className="col-md-6 offset-md-3">
+                <h1>Register</h1>
+                <br />
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <input onChange={handleChange} type="text" name="name" className="form-control" placeholder="Name" value={state.name}/>
+                    </div>
+                    <div className="form-group">
+                        <input onChange={handleChange} type="email" name="email" className="form-control" placeholder="email" value={state.email}/>
+                    </div>
+                    <div className="form-group">
+                        <input onChange={handleChange} type="password" name="password" className="form-control" placeholder="password" />
+                    </div>
+                    <div className="form-group">
+                        <button className="btn btn-outline-warning">{state.buttonText}</button>
+                    </div>
+                </form>
+            </div>
+        </Layout>
     )
 };
 
