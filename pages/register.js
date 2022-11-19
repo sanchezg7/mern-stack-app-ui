84cc976981bdcf9df8a06e4d6fd8a5ecf0d92dcb
Gerardo Sanchez <sanchezg.eng@gmail.com>
2022-11-19 11:17:26 -0500

    ensuring the cors and basic integration with local node api is working. need to do next lesson with validation

--- pages/register.js
+++ pages/register.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Layout from "../components/Layout";
+import axios from "axios";
 
 const register = () => {
 
@@ -10,6 +11,8 @@ const register = () => {
         error: '',
         buttonText: 'Register'
     });
+
+    const { name, email, password } = state;
 
     const handleChange = e => {
         e.preventDefault();
@@ -22,7 +25,19 @@ const register = () => {
 
     const handleSubmit = e => {
         e.preventDefault();
-        console.table(e);
+        // console.table(e);
+        axios.request({
+            // TODO: setup a local proxy to reroute to the address while still having the same route
+            url: "http://localhost:8080/api/register",
+            method: "POST",
+            data: {
+                name,
+                email,
+                password
+            }
+        })
+            .then(response => console.log(response))
+            .catch();
     }
 
     return (
@@ -32,10 +47,10 @@ const register = () => {
                 <br />
                 <form onSubmit={handleSubmit}>
                     <div className="form-group">
-                        <input onChange={handleChange} type="text" name="name" className="form-control" placeholder="Name" value={state.name}/>
+                        <input onChange={handleChange} type="text" name="name" className="form-control" placeholder="Name" value={name}/>
                     </div>
                     <div className="form-group">
-                        <input onChange={handleChange} type="email" name="email" className="form-control" placeholder="email" value={state.email}/>
+                        <input onChange={handleChange} type="email" name="email" className="form-control" placeholder="email" value={email}/>
                     </div>
                     <div className="form-group">
                         <input onChange={handleChange} type="password" name="password" className="form-control" placeholder="password" />
