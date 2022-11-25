63972351c6e8313e5b1314fc2bb8d560671b7971
Gerardo Sanchez <sanchezg.eng@gmail.com>
2022-11-24 20:15:42 -0500

    add notification to success

--- pages/register.js
+++ pages/register.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Layout from "../components/Layout";
 import axios from "axios";
+import { showSuccessMessage, showErrorMessage } from "./register.notification";
 
 const register = () => {
 
@@ -9,10 +10,11 @@ const register = () => {
         email: '',
         password: '',
         error: '',
+        success: '',
         buttonText: 'Register'
     });
 
-    const { name, email, password } = state;
+    const { name, email, password, success, error } = state;
 
     const handleChange = e => {
         e.preventDefault();
@@ -25,7 +27,11 @@ const register = () => {
 
     const handleSubmit = e => {
         e.preventDefault();
-        // console.table(e);
+
+        setState({
+            ...state,
+            buttonText: 'Registering...'
+        });
         axios.request({
             // TODO: setup a local proxy to reroute to the address while still having the same route
             url: "http://localhost:8080/api/register",
@@ -36,12 +42,27 @@ const register = () => {
                 password
             }
         })
-            .then(response => console.log(response))
-            .catch();
+            .then(response => {
+                console.log(response);
+                setState({
+                    ...state,
+                    name: '',
+                    email: '',
+                    password: '',
+                    buttonText: 'Submitted',
+                    success: response.data.message
+                })
+            })
+            .catch(e => {
+                console.error(e);
+                setState({...state, buttonText: 'Register', error: e.response.data.error})
+            });
     }
 
     return (
         <Layout>
+            {success && showSuccessMessage(success)}
+            {error && showErrorMessage(error)}
             <div className="col-md-6 offset-md-3">
                 <h1>Register</h1>
                 <br />
---
+++ pages/register.notification.js
@@ -0,0 +1,11 @@
+export const showSuccessMessage = success => {
+  return(
+      <div className="alert alert-success">{success}</div>
+  );
+};
+
+export const showErrorMessage = error => {
+  return (
+    <div className="alert alert-error">{error}</div>
+  );
+};
