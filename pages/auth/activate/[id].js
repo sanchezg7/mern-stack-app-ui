d0f4bcc4c8956e6feb180de618e41c10b9ef1911
ayogsynergy <sanchezg.eng@gmail.com>
2022-12-08 23:41:38 -0500

    need to finish verifying the account activation functionality then do the account login

--- pages/auth/activate/[id].js
+++ pages/auth/activate/[id].js
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
-import { withRouter } from "next/router";
+import { /*withRouter*/useRouter } from "next/router";
 import jwt from "jsonwebtoken";
 import axios from "axios";
 import Layout from "../../../components/Layout";
 import { showErrorMessage, showSuccessMessage } from "../../register.notification";
 import {API} from "../../../config";
 
-const ActivateAccount = ({ router }) => {
+const ActivateAccount = () => {
+    const router = useRouter();
     const [state, setState] = useState({
         name: '',
         token: '',
@@ -14,22 +15,14 @@ const ActivateAccount = ({ router }) => {
         success: '',
         error: ''
     });
-    // let token = router.query.id;
 
-    const { name, token, buttonText, success, error } = state;
-
-    useEffect(() => {
-        // let token = router.query.id;
-        console.log(router.query.id);
-        if(token) {
-            const { name } = jwt.decode(token);
-            setState({...state, name});
-        }
-    }, []);
+    const { name, buttonText, success, error } = state;
 
     const handleSubmit = async e => {
         e.preventDefault();
         setState({...state, buttonText: "Activating"});
+        // had to access the router query param here because it wasn't happening in the root render scope of the component
+        let token = router.query.id;
         try{
             const response = await axios.post(`${API}/register/activate`, {
                   token
@@ -41,23 +34,23 @@ const ActivateAccount = ({ router }) => {
         }
     };
 
-    // return (
-    //     <Layout>
-    //         <div className="row">
-    //             <div className="col-md-6 offset-md-3">
-    //                 <h1>Hello {name}, let's activate your account.</h1>
-    //                 {success && showSuccessMessage(success)}
-    //                 {error && showErrorMessage(error)}
-    //                 <button
-    //                     className="btn btn-outline-warning btn-block"
-    //                     onClick={handleSubmit}
-    //                 >{buttonText}</button>
-    //             </div>
-    //
-    //         </div>
-    //     </Layout>
-    // )
+    return (
+        <Layout>
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <h1>Hello {name}, let's activate your account.</h1>
+                    {success && showSuccessMessage(success)}
+                    {error && showErrorMessage(error)}
+                    <button
+                        className="btn btn-outline-warning btn-block"
+                        onClick={handleSubmit}
+                    >{buttonText}</button>
+                </div>
+
+            </div>
+        </Layout>
+    )
     return <div>{JSON.stringify(router)}</div>
 };
 
-export default withRouter(ActivateAccount);
+export default ActivateAccount;
